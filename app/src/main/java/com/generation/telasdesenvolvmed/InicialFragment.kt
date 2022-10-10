fe0a1e9b948e48ed0f492c87e76435f8b6bfa123
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-10-09 21:50:15 -0300

    Adicionando Room ao projeto e garantindo Login Automático

--- app/src/main/java/com/generation/telasdesenvolvmed/InicialFragment.kt
+++ app/src/main/java/com/generation/telasdesenvolvmed/InicialFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.generation.telasdesenvolvmed.databinding.FragmentInicialBinding
 
 
 class InicialFragment : Fragment() {
-
+    private lateinit var binding: FragmentInicialBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicial, container, false)
+        binding = FragmentInicialBinding.inflate(layoutInflater, container, false)
+
+        //findNavController().navigate(R.id.action_inicialFragment_to_postFragment)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/generation/telasdesenvolvmed/LoginFragment.kt
+++ app/src/main/java/com/generation/telasdesenvolvmed/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.generation.telasdesenvolvmed.data.Login
 import com.generation.telasdesenvolvmed.databinding.FragmentLoginBinding
 
 
@@ -93,6 +94,11 @@ class LoginFragment : Fragment() {
                             "Login Bem Sucedido",
                             Toast.LENGTH_SHORT
                         ).show()
+                        mainViewModel.addLogin(
+                            Login(0,
+                                mainViewModel.cadastroVerificado.value?.body()!!.email,
+                                mainViewModel.cadastroVerificado.value?.body()!!.senha)
+                        )
                         findNavController().navigate(R.id.action_loginFragment_to_postFragment)
                     } else {
                         Toast.makeText(context, "Senha Incorreta", Toast.LENGTH_SHORT).show()
